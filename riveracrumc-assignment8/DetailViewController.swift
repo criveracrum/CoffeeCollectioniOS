5a7893135ec804add8f0c5ec5e65c9749d4a4644
Chad <criveracrum@gmail.com>
2021-03-01 11:35:42 -0600

    added detailed view

--- riveracrumc-assignment8/DetailViewController.swift
+++ riveracrumc-assignment8/DetailViewController.swift
@@ -9,10 +9,33 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var originLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var tasteLabel: UILabel!
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    @IBOutlet weak var photo: UIImageView!
+    var coffee : Coffee?;
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        if let cof = coffee {
+            titleLabel.text = cof.name
+            originLabel.text = cof.origin
+            tasteLabel.text = cof.tastingNotes;
+            infoLabel.text = cof.description;
+            
+            photo.image = UIImage(named: cof.photo)
+            
+        }
     }
     
 
